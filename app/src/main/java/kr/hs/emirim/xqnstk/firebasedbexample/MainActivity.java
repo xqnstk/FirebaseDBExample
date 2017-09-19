14143004c17196f1f907fef23500bf254a9c5905
xqnstk <xqnstk@gmail.com>
2017-09-19 19:57:16 +0900

    #2 참조연결

--- app/src/main/java/kr/hs/emirim/xqnstk/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xqnstk/firebasedbexample/MainActivity.java
@@ -2,45 +2,25 @@ package kr.hs.emirim.xqnstk.firebasedbexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    static final String TAG="파베:MainActivity";
+
+    private TextView mNameTextView;
+    private TextView mGithubTextView;
+    private ImageView mProfileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        mNameTextView = (TextView) findViewById(R.id.profile_name);
+        mGithubTextView = (TextView) findViewById(R.id.profile_github);
+        mProfileImageView = (TextView) findViewById(R.id.profile_image);
 
-        myRef.setValue("Hello, World!");
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-                Toast.makeText(getApplication(), value, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
     }
 }
